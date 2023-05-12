edf48cd1b30d2a6eb098930f6fc2bdb72e32e9fb
jlinkola23 <123981361+jlinkola23@users.noreply.github.com>
2023-05-12 14:37:08 -0400

    Update Withdraw.java
    current balance updated

--- src/Withdraw.java
+++ src/Withdraw.java
@@ -1,4 +1,7 @@
 public class Withdraw {
     // funds available for withdrawal
     // withdrawal amount: $350
+    
+    // current balance after withdrawal: $1850
+    // thank you for banking with "Bank of America"
 }
